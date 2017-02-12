54b57dc700a170485ae43f66a9d0a66f4d47ae4d
srozb <github@rozbicki.eu>
2017-02-12 20:01:50 +0100

    run.sh fix

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker run -d --cap-add SYS_PTRACE \
            --net=host \
            -v /proc:/host/proc:ro \
            -v /sys:/host/sys:ro \
-           --name net-data \
-           srozb/net-data
+           --name netdata \
+           srozb/netdata
 
